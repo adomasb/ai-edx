7abba8076dae081093a901ed0276131e9bbf739e
adomasb <adomasbudrys@gmail.com>
2015-02-09 17:47:01 +0200

    init with finished refresher

--- refresher/shopSmart.py
+++ refresher/shopSmart.py
@@ -4,7 +4,7 @@
 # educational purposes provided that (1) you do not distribute or publish
 # solutions, (2) you retain this notice, and (3) you provide clear
 # attribution to UC Berkeley, including a link to http://ai.berkeley.edu.
-# 
+#
 # Attribution Information: The Pacman AI projects were developed at UC Berkeley.
 # The core projects and autograders were primarily created by John DeNero
 # (denero@cs.berkeley.edu) and Dan Klein (klein@cs.berkeley.edu).
@@ -28,8 +28,15 @@ def shopSmart(orderList, fruitShops):
         orderList: List of (fruit, numPound) tuples
         fruitShops: List of FruitShops
     """
-    "*** YOUR CODE HERE ***"
-    return None
+    pos = 0
+    max = len(fruitShops)
+    for index in range(max):
+       if fruitShops[0].getPriceOfOrder(orderList) < fruitShops[index].getPriceOfOrder(orderList):
+           pos = 0
+       else:
+           pos = index
+    return fruitShops[pos]
+
 
 if __name__ == '__main__':
   "This code runs when you invoke the script from the command line"
